6612273d4c950fd23837be1a97d70f3ffefc4800
Павел <pashanoy@gmail.com>
2019-10-07 20:43:53 +0300

    Add tests

---
+++ src/test/java/AppMainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppMainTest {
+    @Test
+    void TestInit() {
+        AppMain app = new AppMain();
+
+        app.simpleInitApp();
+    }
+}
---
+++ src/test/java/CannonballTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CannonballTest {
+    @Test
+    void TestGravityIncrement() {
+        Cannonball cannonball = new Cannonball();
+        cannonball.gravityUp();
+        cannonball.gravityUp();
+        
+        assertEquals(3, cannonball.getGravity());
+    }
+
+    @Test
+    void TestGravityIncrementMax() {
+        Cannonball cannonball = new Cannonball();
+        for(int i = 0; i < 15; i++){
+            cannonball.gravityUp();
+        }
+        
+        assertEquals(10, cannonball.getGravity());
+    }
+
+    @Test
+    void TestGravityDecrement() {
+        Cannonball cannonball = new Cannonball();
+        cannonball.gravityUp();
+        cannonball.gravityUp();
+
+        cannonball.gravityDown();
+
+        assertEquals(2, cannonball.getGravity());
+    }
+
+    @Test
+    void TestGravityDecrementMin() {
+        Cannonball cannonball = new Cannonball();
+        cannonball.gravityUp();
+        cannonball.gravityUp();
+
+        for(int i = 0; i < 15; i++){
+            cannonball.gravityDown();
+        }
+
+        assertEquals(1, cannonball.getGravity());
+    }
+}
